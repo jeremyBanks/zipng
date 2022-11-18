d9d6dc0511e1b939842d52f80212e0e00df3001e
Jeremy Banks <_@jeremy.ca>
2022-11-18 03:27:06 -0500

    bad sub-byte alignment

--- examples/text.rs
+++ examples/text.rs
@@ -22,7 +22,6 @@ fn main() -> Result<(), panic> {
     let font = Mini5pt;
     let width = font.width();
     for (character, bitmap) in font.glyphs() {
-        let bits = font.width() * font.height();
         data.extend(bitmap.to_be_bytes());
         data.extend([0x00; 1]);
         data.extend([0xFF; 1]);
@@ -44,18 +43,18 @@ fn main() -> Result<(), panic> {
     } = ZipngOptions::default_for_data(&data);
     let color_palette = color_palette.as_deref();
 
-    // let bit_depth = EightBit;
-    // let color_mode = Indexed;
-    // let color_palette = Some(PALLETTE_8_BIT_DATA.as_slice());
+    let bit_depth = EightBit;
+    let color_mode = Indexed;
+    let color_palette = Some(PALLETTE_8_BIT_DATA.as_slice());
 
     // let bit_depth = EightBit;
     // let color_mode = RedGreenBlue;
     // let color_palette = None::<&[u8]>;
 
-    let bit_depth = OneBit;
-    let color_mode = Indexed;
-    let color_palette = [0xFF_u8, 0xFF, 0xEE, 0x11, 0x11, 0x33];
-    let color_palette = Some(&color_palette[..]);
+    // let bit_depth = OneBit;
+    // let color_mode = Indexed;
+    // let color_palette = [0xFF_u8, 0xFF, 0xEE, 0x11, 0x11, 0x33];
+    // let color_palette = Some(&color_palette[..]);
 
     let bits_per_pixel = bit_depth.bits_per_sample() * color_mode.samples_per_pixel();
     let pixels = data.len() * 8 / bits_per_pixel;
